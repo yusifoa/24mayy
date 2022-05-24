2d1085c63a9bbd5b1bf7d234edd6fff20516d42e
Yusif O. Abilov <yusifoa@code.local>
2022-05-24 18:23:23 +0400

    24mayy

---
+++ 24may/24may/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _24may
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person human = new Person("Yusif", "Abilov", 10);
+           
+            
+        }
+    }
+}
---
+++ 24may/24may/isAdult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24may
+{
+    class Person
+    {
+        public string Name;
+        public string Surname;
+        public int Age;
+        public bool isAdult;
+
+        public Person(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            isAdult = true;
+            if (Age > 18)
+            {
+                Console.WriteLine(isAdult);
+            }
+            else
+            {
+                Console.WriteLine(!isAdult);
+            }
+        }
+    }
+}
